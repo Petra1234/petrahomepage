7df52950b11d513875abf73d7b4b37764aa6064b
Wolfgang <wolfgang.heindl.wh@gmail.com>
2017-12-03 08:11:28 +0100

    Update Course-Data

--- js/data.js
+++ js/data.js
@@ -4,11 +4,42 @@ var petrasEvents = {
 	'10-16-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'10-23-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'10-30-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	
 	'11-06-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'11-13-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'11-20-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'11-27-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
 	'12-04-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'12-11-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
 	'12-18-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'01-08-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'01-15-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'01-22-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'01-29-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'02-19-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'02-26-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'03-05-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'03-12-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'03-15-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'03-19-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'04-09-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'04-16-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'04-23-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'04-30-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'05-07-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'05-14-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'05-28-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'06-04-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'06-11-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'06-18-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+	'06-25-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>',
+
+	'07-02-2017' : '<a class="red"  href="#yogaBeginner">19:00 - 20:30 Yoga - Anfänger </a>'
 };
